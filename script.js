f6560a03ccef7741f6e82e4328b094386ece488b
sheerlore <forgivemyblue13@gmail.com>
2024-06-02 03:07:40 +0900

    add comment

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const cards  = document.querySelectorAll(".card"),
-timeTag = document.querySelector(".time b"),
-flipsTag = document.querySelector(".flips b"),
-refreshBtn = document.querySelector(".details button");
+// HTMLElements
+const cards = document.querySelectorAll(".card"),
+    timeTag = document.querySelector(".time b"),
+    flipsTag = document.querySelector(".flips b"),
+    refreshBtn = document.querySelector(".details button");
 
-let maxTime = 20;
+// States
+let maxTime = 25;
 let timeLeft = maxTime;
 let flips = 0;
 let matchedCards = 0;
 let disableDeck = false;
 let isPlaying = false;
-let cardOne, cardTwo, timer;
+let cardOne, cardTwo;
+let timer;
 
+// Function
 function initTimer() {
-    if (timeLeft <= 0) { 
+    if (timeLeft <= 0) {
         // [終了]：時間切れ
         return clearInterval(timer);
     }
@@ -20,7 +24,7 @@ function initTimer() {
     timeTag.innerText = timeLeft;
 }
 
-function flipCard({target: clickedCard}) {
+function flipCard({ target: clickedCard }) {
     if (!isPlaying) {
         isPlaying = true;
         timer = setInterval(initTimer, 1000);
@@ -34,16 +38,16 @@ function flipCard({target: clickedCard}) {
         }
         cardTwo = clickedCard;
         disableDeck = true;
-        let cardOneIcon =cardOne.querySelector(".back-view i").classList.value,
-        cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
+        let cardOneIcon = cardOne.querySelector(".back-view i").classList.value,
+            cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
         matchCards(cardOneIcon, cardTwoIcon);
     }
 }
 
-function matchCards(icon1, icon2){
-    if(icon1 === icon2){
+function matchCards(icon1, icon2) {
+    if (icon1 === icon2) {
         matchedCards++;
-        if(matchedCards  == 6 && timeLeft > 0) {
+        if (matchedCards == 6 && timeLeft > 0) {
             // [終了]：すべてのカードを当てられたとき
             return clearInterval(timer);
         }
@@ -54,15 +58,15 @@ function matchCards(icon1, icon2){
         return disableDeck = false;
     }
 
-    setTimeout(()=>{
+    setTimeout(() => {
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
-    },400)
+    }, 400)
 
     setTimeout(() => {
-        cardOne.classList.remove("shake","flip");
-        cardTwo.classList.remove("shake","flip");
-        cardOne = cardTwo= "";
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
         disableDeck = false;
     }, 1200);
 }
@@ -88,12 +92,14 @@ function shuffleCards() {
     })
 }
 
-shuffleCards();
+function main() {
+    // INIT
+    shuffleCards();
+    refreshBtn.addEventListener("click", shuffleCards);
+    cards.forEach((card) => {
+        card.addEventListener("click", flipCard);
+    });
+}
 
-refreshBtn.addEventListener("click", shuffleCards);
-
-console.log(cards.length)
-cards.forEach((card) => {
-    card.addEventListener("click", flipCard);
-});
+main();
 
